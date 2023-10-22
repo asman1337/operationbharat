ddcb64ae046ce997a2860da1ed093992287683a0
Asman Mirza <rambo007.am@gmail.com>
2023-10-22 15:17:20 +0530

    minor fixes

--- src/main/kotlin/com/b3mgames/operationbharat/backend/config/SecurityConfig.kt
+++ src/main/kotlin/com/b3mgames/operationbharat/backend/config/SecurityConfig.kt
@@ -11,7 +11,6 @@ package com.b3mgames.operationbharat.backend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
